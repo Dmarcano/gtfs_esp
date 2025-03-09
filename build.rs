7d733b975db14498fb8805ed2b42942dfc7f2383
Diego Marcano <marcanp.diego@gmail.com>
2025-03-09 17:10:49 -0400

    init

---
+++ build.rs
@@ -0,0 +1,16 @@
+use dotenvy_macro::dotenv;
+
+fn main() {
+
+   let ssid =  dotenv!("SSID");
+   let password =  dotenv!("PASSWORD");
+
+    println!("cargo:rustc-env=SSID={ssid}");
+    println!("cargo:rustc-env=PASSWORD={password}");
+    
+
+    println!("cargo:rustc-link-arg=-Tlinkall.x");
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
+
+
+}
